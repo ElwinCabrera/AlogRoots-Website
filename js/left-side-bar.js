89a48b0b3309a4e86d429115fa29ca84a0087cac
Elwin Cabrera <elwincab0@gmail.com>
2019-10-08 18:21:50 -0400

    added a cool sidebar that i found online and started to modify already. Its under the MIT LICENCEso we good to go I added the licence and where i got it on the parts that I used it

---
+++ js/left-side-bar.js
@@ -0,0 +1,45 @@
+/*Copyright (c) 2019 - Elwin Cabrera - https://codepen.io/davigsouza81/pen/VrYJEX 
+
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall 
+be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE. */
+
+$(document).ready(function() {
+    $("#sidebar").niceScroll({
+        cursorcolor: '#53619d',
+        cursorwidth: 4,
+        cursorborder: 'none'
+    });
+
+    $('.overlay').on('click', function() {
+        $('#sidebarCollapse').addClass('active');
+        $('#sidebar').removeClass('active');
+        $('.overlay').fadeOut();
+    });
+
+    $('#sidebarCollapse').on('click', function() {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $('#sidebar').addClass('active');
+            $('.overlay').fadeIn();
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        }
+    });
+});
