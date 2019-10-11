c416a06919726beb2af7d02706dceab624a2d1a5
Elwin Cabrera <elwincab0@gmail.com>
2019-10-10 22:03:58 -0400

    Started to intergrate things to work with django and also finally decided on a licence that works for me (Mozzila Public Licence 2.0) Will add in details later

--- django/landing_pg/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LandingPgConfig(AppConfig):
-    name = 'landing_pg'
--- django/landing_pg/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from landing_pg import views
-
-urlpatterns = [ 
-    path('', views.main_landing, name='main_landing')
-]
