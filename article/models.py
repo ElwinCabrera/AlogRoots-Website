721940fef145679ef492c97ed86d00f6e8c74b64
Elwin Cabrera <elwincab0@gmail.com>
2019-10-12 19:35:35 -0400

    added all the models from the app article to the admin console and updated the models file and the database for that same app

--- article/models.py
+++ article/models.py
@@ -8,17 +8,17 @@ class Article(models.Model):
     articleID = models.AutoField(primary_key=True)
 
     postDate = models.DateTimeField(auto_now=True, auto_now_add=False)
-    category = models.CharField(max_length=120);
-    title = models.CharField(max_length=120);
-    imagesPath = models.FilePathField(path="/static/images/article{0}".format(articleID), match='*.*', recursive=True, max_length=100)
-    preamble = models.TextField()
+    category = models.CharField(max_length=120, blank=True, default="!--Blank Article Category--!");
+    title = models.CharField(max_length=120, blank=True, default="!--Blank Article Title--!");
+    imagesPath = models.FilePathField(path="static/images/article{0}".format(articleID), blank=True ,recursive=True, max_length=100)
+    preamble = models.TextField(blank=True, default="!--Blank Article Preamble--!")
     
 
 class Section(models.Model):
     sectionId = models.AutoField(primary_key=True)
     
-    title = models.CharField(max_length=120);
-    secText = models.TextField()
+    title = models.CharField(max_length=120,  blank=True, default="!--Blank Section Title--!");
+    secText = models.TextField( blank=True, default="!--Blank Section Text--!")
     hasSubSections = models.BooleanField(default=False)
 
     article = models.ForeignKey(Article,  on_delete=models.CASCADE, related_name="section")
@@ -26,7 +26,7 @@ class Section(models.Model):
 
 class SubSection(models.Model):
     sectionId = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=120);
-    subSecText = models.TextField()
-    section = models.ForeignKey(Section,  on_delete=models.CASCADE, related_name="subSec")
+    title = models.CharField(max_length=120, blank=True, default="!--Blank Sub Section Title--!");
+    subSecText = models.TextField( blank=True, default="!--Blank Sub Section Text--!")
+    section = models.ForeignKey(Section,  on_delete=models.CASCADE, related_name="subSec", )
 
