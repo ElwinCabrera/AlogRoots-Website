9ec982c14e8be5c5e62c55c760d7d9f6fc1e29bc
Elwin Cabrera <elwincab0@gmail.com>
2019-10-12 11:29:52 -0400

    fixed some errors and created a superuser to test

--- article/models.py
+++ article/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.ForeignKey
+#from django.db.models.ForeignKey
 
 # Create your models here.
 
@@ -10,23 +10,23 @@ class Article(models.Model):
     postDate = models.DateTimeField(auto_now=True, auto_now_add=False)
     category = models.CharField(max_length=120);
     title = models.CharField(max_length=120);
-    imagesPath = models.FilePathField(path="/static/images/article{0}".format(articleID), match=None, recursive=recursive, max_length=100)
+    imagesPath = models.FilePathField(path="/static/images/article{0}".format(articleID), match='*.*', recursive=True, max_length=100)
     preamble = models.TextField()
     
 
-class Section():
+class Section(models.Model):
     sectionId = models.AutoField(primary_key=True)
     
     title = models.CharField(max_length=120);
-    text = preamble = models.TextField()
+    secText = models.TextField()
     hasSubSections = models.BooleanField(default=False)
 
-    article = models.ForeignKey(Article,  on_delete=models.PROCTECT, related_name="section")
+    article = models.ForeignKey(Article,  on_delete=models.CASCADE, related_name="section")
 
 
-class SubSection():
+class SubSection(models.Model):
     sectionId = models.AutoField(primary_key=True)
     title = models.CharField(max_length=120);
-    text = preamble = models.TextField()
-    section = models.ForeignKey(Section,  on_delete=models.PROCTECT, related_name="subSec")
+    subSecText = models.TextField()
+    section = models.ForeignKey(Section,  on_delete=models.CASCADE, related_name="subSec")
 
