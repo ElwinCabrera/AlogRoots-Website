76ddcfc7f1b23d596fe4374ddb5443b8c271fedf
Elwin Cabrera <elwincab0@gmail.com>
2019-10-13 10:52:02 -0400

    deleted unneeded text from navbar in the html. Also added a some more needed fields and two more tables to the models file in article

--- article/models.py
+++ article/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class Article(models.Model):
+class Topic(models.Model):
 
     articleID = models.AutoField(primary_key=True)
 
@@ -12,6 +12,17 @@ class Article(models.Model):
     title = models.CharField(max_length=120, blank=True, default="!--Blank Article Title--!");
     imagesPath = models.FilePathField(path="static/images/article{0}".format(articleID), blank=True ,recursive=True, max_length=100)
     preamble = models.TextField(blank=True, default="!--Blank Article Preamble--!")
+
+    time_worst = models.CharField(max_length=1, blank=False, default=' ')
+    time_average = models.CharField(max_length=1, blank=False, default=' ')
+    time_best = models.CharField(max_length=1, blank=False, default=' ')
+
+    space_worst = models.CharField(max_length=1, blank=False, default=' ')
+    space_average = models.CharField(max_length=1, blank=False, default=' ')
+    space_best = models.CharField(max_length=1, blank=False, default=' ')
+
+    def __str__(self):
+        return "id: "+self.articleID + "-" + self.title 
     
 
 class Section(models.Model):
@@ -21,12 +32,38 @@ class Section(models.Model):
     secText = models.TextField( blank=True, default="!--Blank Section Text--!")
     hasSubSections = models.BooleanField(default=False)
 
-    article = models.ForeignKey(Article,  on_delete=models.CASCADE, related_name="section")
+    topic = models.ForeignKey(Topic,  on_delete=models.CASCADE, related_name="section")
+
+    def __str__(self):
+        return "id: "self.sectionId+ "-"self.title +"({0})".format(Topic.title)
+    
 
 
 class SubSection(models.Model):
-    sectionId = models.AutoField(primary_key=True)
+    subSectionId = models.AutoField(primary_key=True)
     title = models.CharField(max_length=120, blank=True, default="!--Blank Sub Section Title--!");
     subSecText = models.TextField( blank=True, default="!--Blank Sub Section Text--!")
     section = models.ForeignKey(Section,  on_delete=models.CASCADE, related_name="subSec", )
 
+    def __str__(self):
+        return "id: "self.subSectionID+ "-"self.title +"({0})".format(Section.title)
+
+
+class GoodFor(models.Model):
+    item1 = models.CharField(max_length=120, blank=True, default=" ");
+    item2 = models.CharField(max_length=120, blank=True, default=" ");
+    item3 = models.CharField(max_length=120, blank=True, default=" ");
+    item4 = models.CharField(max_length=120, blank=True, default=" ");
+    item5 = models.CharField(max_length=120, blank=True, default=" ");
+
+    topic = models.ForeignKey(Topic,  on_delete=models.CASCADE, related_name="good-for")
+
+class NotGoodFor(models.Model):
+    item1 = models.CharField(max_length=120, blank=True, default=" ");
+    item2 = models.CharField(max_length=120, blank=True, default=" ");
+    item3 = models.CharField(max_length=120, blank=True, default=" ");
+    item4 = models.CharField(max_length=120, blank=True, default=" ");
+    item5 = models.CharField(max_length=120, blank=True, default=" ");
+
+    topic = models.ForeignKey(Topic,  on_delete=models.CASCADE, related_name="not-good-for")
+    
